1c8866eeacd9f6dbec48974395a27a60656d7263
Tommaso Siligardi <tomesse@outlook.it>
2021-02-19 20:54:09 +0100

    commit

--- index.js
+++ index.js
@@ -2,30 +2,28 @@ const sqlite3 = require("sqlite3")
 const express = require ("express")
 const app = new express()
 app.use(express.json())
-const db = new sqlite3.Database('./data.sqlite3')
+const db = new sqlite3.Database("./data.sqlite3")
 
 db.run("CREATE TABLE IF NOT EXISTS chiamate (ip TEXT, n INT)")
 
-app.get("/",(req,res)=>{
-    const ip = req.ip
-    db.get("SELECT * FROM chiamate WHERE ip = ? ",ip,(err,row)=>{
-        if (err) {
-            console.log(err)
-            return res.status(500).json({error: "error"})
-        }
-        console.log(row)
-        if (row){
-            res.status(200).json({n : row.n+1})
-            db.run("UPDATE chiamate SET n = ? WHERE ip = ? ",row.n+1,ip)
-            console.log("esiste")
-        }
-        else{
-            db.run("INSERT INTO chiamate VALUES (?,?)",ip,1)
-            res.status(200).json({n : 1})
-        }
-    })
+app.get("/", (req, res) => {
+  const ip = req.ip
+  db.get("SELECT * FROM chiamate WHERE ip = ? ", ip, (err, row) => {
+    if (err) {
+      console.log(err)
+      return res.status(500).json({ error: "error" })
+    }
+
+    if (row) {
+      res.status(200).json({ n: row.n + 1 })
+      db.run("UPDATE chiamate SET n = ? WHERE ip = ? ", row.n + 1, ip)
+    } else {
+      db.run("INSERT INTO chiamate VALUES (?,?)", ip, 1)
+      res.status(200).json({ n: 1 })
+    }
+  })
 
 })
 
 
-app.listen(8080,()=>console.log("server listening on port 8080"))
+app.listen(8080, () => console.log("server listening on port 8080"))
